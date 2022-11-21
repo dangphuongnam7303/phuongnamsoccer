5baf921e0d2c74c2483a9f1e6f855ba847ae9484
DangPhuongNam7303 <phuongnam7303@gmail.com>
2022-11-21 13:40:08 +0700

    done

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-require_once '/xampp/htdocs/DAM_FALL2022/site/trang-chinh/index.php';
+require_once '/xampp/htdocs/DAM_FALL2022/global.php';
+header('location: /DAM_FALL2022/site/trang-chinh/index.php');
 ?>
